9009bb78b7d30243a0009156590599a8802a191d
Lailaebrahim <ebrahimlaila815@gmail.com>
2024-06-06 14:35:05 +0300

    Use API

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $.ajax({
+    type: 'GET',
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    success: function (data) {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    }
+  });
+});
+
